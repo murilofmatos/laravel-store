ee82612963f9b9afcdf908d4e3a5e18fa96a39b2
Murilo Francisco Matos <murilofm.social@gmail.com>
2024-12-12 15:58:41 +0000

    Fix covers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
         <div class="flex flex-wrap -m-4">
             @foreach ($products as $product)
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-48 rounded overflow-hidden">
-                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ \Illuminate\Support\Facades\Storage::url($product->cover) }}">
+                <a href="{{ route('product.show', $product->slug) }}" class="block relative h-48 rounded overflow-hidden">
+                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="{{ $product->cover ? \Illuminate\Support\Facades\Storage::url($product->cover) : '/assets/default.webp' }}">
                 </a>
                 <div class="mt-4">
                     <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
